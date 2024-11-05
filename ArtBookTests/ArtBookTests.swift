f5b40675d261c34adb2eca86a12f0197ab9173d8
feyzackgzglu <feyza.ackgzgluu@gmail.com>
2024-11-05 14:16:29 +0300

    Initial Commit

---
+++ ArtBookTests/ArtBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBookTests.swift
+//  ArtBookTests
+//
+//  Created by Feyza on 30.10.2024.
+//
+
+import Testing
+@testable import ArtBook
+
+struct ArtBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
